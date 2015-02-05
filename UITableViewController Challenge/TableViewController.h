5c3b30110b72e618bb7d933b395ca89c9bae8cda
Victor Mendonça Lellis <vmlellis@Victors-Mac.local>
2015-02-05 16:14:27 -0200

    solved the challenge

--- UITableViewController Challenge/TableViewController.h
+++ UITableViewController Challenge/TableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  TableViewController.h
 //  UITableViewController Challenge
 //
 //  Created by Victor Mendonça Lellis on 2/5/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface TableViewController : UITableViewController
 
 @end
-
